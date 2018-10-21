6495a8ccc18cac97438c02fc09b6c7e6a8939f72
t <t>
2018-10-21 15:33:51 +0200

    Solved task 2.

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,8 @@ mod event_loop;
 mod types;
 mod ui;
 
-use audioengine::types::KeyAction;
+use audioengine::types::{KeyAction, SignalBuffer};
+use types::{GraphEvent, GraphEventType};
 use ui::Ui;
 
 #[allow(unused_imports)]
@@ -17,14 +18,38 @@ use std::f64::consts::PI;
 fn main() -> Result<(), Error> {
     let audioengine = audioengine::EngineController::start();
 
+    let (sender, receiver) = std::sync::mpsc::channel::<GraphEvent>();
+    let mut signal_buffer = SignalBuffer::new();
+
+    let mut phase = 0.0;
+
     let synth = move |_t: f64, _dt: f64, _action: Option<KeyAction>| {
-        // TODO: Implement your synthesizer here
-        0.0
+        let freq = 440.0;
+        phase += freq * _dt * 2.0 * PI;
+
+        let mut phase_crossed_zero = false;
+        if phase > PI {
+            phase -= 2.0 * PI;
+            phase_crossed_zero = true;
+        }
+
+        let my_value = phase.sin();
+
+        signal_buffer.push_back(my_value);
+
+        if phase_crossed_zero {
+            sender.send((GraphEventType::SignalGraph,
+            signal_buffer.clone(),
+            4410));
+            signal_buffer.clear();
+        }
+
+        my_value
     };
 
     audioengine.set_processor_function(Box::new(synth));
 
-    let mut window = Ui::new("Ljubljana", [1280.0, 800.0], audioengine, None, None, None);
+    let mut window = Ui::new("Ljubljana", [1280.0, 800.0], audioengine, None, None, Some(receiver));
 
     window.show();
 
