602b7baf6aefe1f093c3b9682da7fd90a740dc9e
t <t>
2019-03-13 18:41:58 +0100

    Implemented task 5.

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,51 @@ fn transform_key_action(action: Option<i32>) -> Option<f64> {
     action.map(|v| C_FREQUENCY * HALFSTEP_EXP.powi(v))
 }
 
+#[derive(Debug, Clone, Copy)]
+enum ADSRState {
+    Attack,
+    Decay,
+    Release
+}
+
+struct ADSR {
+    state: ADSRState,
+    value: f64,
+    pub attack: f64,
+    pub decay: f64,
+    pub sustain: f64,
+    pub release: f64
+}
+
+impl ADSR {
+    pub fn new() -> Self {
+        Self {
+            state: ADSRState::Release,
+            value: 0.0,
+            attack: 10000.0,
+            decay: 10000.0,
+            sustain: 0.1,
+            release: 10000.0
+        }
+    }
+
+    pub fn process(&mut self, gate: f64) -> f64 {
+        let trigger = gate >= 0.5;
+        self.value = match self.state {
+            ADSRState::Attack => (self.value + (gate / self.attack)).min(1.0),
+            ADSRState::Decay => self.value - ((self.value - self.sustain) / self.decay),
+            ADSRState::Release => self.value - (self.value / self.release)
+        };
+        self.state = match (self.state, trigger) {
+            (_, false) => ADSRState::Release,
+            (ADSRState::Attack, true) => if self.value > 0.99 { ADSRState::Decay } else { ADSRState::Attack },
+            (ADSRState::Decay, true) => ADSRState::Decay,
+            (ADSRState::Release, true) => ADSRState::Attack
+        };
+        self.value
+    }
+}
+
 #[allow(unused_variables, unused_assignments)]
 fn main() -> Result<(), Error> {
     let audioengine = audioengine::EngineController::start();
@@ -36,6 +81,7 @@ fn main() -> Result<(), Error> {
     let mut time = 0.0;
     let mut phase = 0.0;
     let mut freq = 440.0;
+    let mut adsr = ADSR::new();
 
     let (sender, receiver) = std::sync::mpsc::channel::<GraphEvent>();
     let mut signal_buffer = SignalBuffer::new();
@@ -59,7 +105,7 @@ fn main() -> Result<(), Error> {
             None => 0.0
         };
 
-        let my_value = phase.sin() * gate;
+        let my_value = phase.sin() * adsr.process(gate);
 
         signal_buffer.push_back(my_value);
 
